893bec005b021e7fb350bb56b59a3edb857994a7
Ichiomacode <ichiomafaustina@gmail.com>
2023-05-11 00:56:57 +0300

    add details

--- star-wars/src/App.jsx
+++ star-wars/src/App.jsx
@@ -1,13 +1,22 @@
+/** @format */
 import './App.css';
 import CardList from './Components/CardList';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import More from "./Components/More";
 
 function App() {
- return (
-  <div>
-    <CardList/>
-  </div>
- )
+  return (
+    <div>
+      <Header className="header" />
+      <main>
+        <Routes>
+          <Route path="/" element={<CardList />} />
+          <Route path="/movie/:id" element={<More/>} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
